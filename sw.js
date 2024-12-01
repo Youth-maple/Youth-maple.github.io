1d587790f2bc3417b95e6375b31b76f115bc2135
DTbreeze <17879738610@163.com>
2024-12-01 15:29:35 +0800

    Site updated: 2024-12-01 15:29:35

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732976086500";
+const VERSION = "1733038165661";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
